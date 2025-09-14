289394230939b44682d313007cf63e36a9b0232d
bin0518ho-eng <bin0518ho@gmail.com>
2025-09-14 14:59:23 +0900

    Add files via upload

---
+++ 091402.py
@@ -0,0 +1,10 @@
+friends = ["래현", "민호", "헌서"]
+
+friends.insert(1,"재청")
+
+print(friends)
+name=friends.pop() # 제일 마지막 요소를 뽑아줌
+print("나간사람: ", name)
+print(friends)
+friends.remove("래현") # 지정돤 요소를 제거할수 있음
+print(friends)
